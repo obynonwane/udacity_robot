5e6b9fc77f13268b7cc2103bb42da7f3d011c715
obinna <biostechnologyng@gmail.com>
2022-12-03 22:42:13 +0100

    Update

--- index.js
+++ index.js
@@ -8,34 +8,50 @@
 //8. Constructor - done
 
 class Robot {
-    //defined instance properties
-    name = "Alicia";
-    height = "6.5ft";
-    color = "brown";
-  
-    //defined an array
-    listofClothes = ["black", "red", "purple"];
-  
-    constructor() {}
-  
-    //defining a function
-    findClothSpecified() {
-      //define a loop
-      for (let i = 0; i < this.listofClothes.length; i++) {
-        //define a conditions
-        if (this.listofClothes[i] == "yellow") {
-          console.log(`Hi master i found the ${this.listofClothes[i]} cloth`);
-        } else {
-          console.log(
-            `Hi master i found the ${this.listofClothes[i]} cloth, seems yellow does not exist`
-          );
-        }
+  //defined instance properties
+  //   name = "Alicia";
+  //   height = "6.5ft";
+  //   color = "brown";
+
+  robotProperties = {
+    name: "Alicia",
+    height: "6.5ft",
+    color: "brown",
+  };
+
+  //defined an array
+  listofClothes = ["black", "yellow", "red", "purple"];
+
+  constructor(_name, _height, _color) {
+    this.robotProperties.name = _name;
+    this.robotProperties.height = _height;
+    this.robotProperties.color = _color;
+  }
+
+  //defining a function
+  findClothSpecified() {
+    //define a loop
+    for (let i = 0; i < this.listofClothes.length; i++) {
+      //define a conditions
+      if (this.listofClothes[i] == "yellow") {
+        console.log(`Initializing Robot ${this.robotProperties.name}`);
+        console.log(`My height is ${this.robotProperties.height}`);
+        console.log(`my color is ${this.robotProperties.color}`);
+        console.log(`Hi master i found the ${this.listofClothes[i]} cloth`);
+        this.bringClothToTheRoom();
+        return;
+      } else {
+        console.log(
+          `Hi master i found the ${this.listofClothes[i]} cloth, seems yellow does not exist`
+        );
       }
     }
-  
-    // async bringClothToTheRoom() {}
   }
-  
-  const alicia = new Robot();
-  console.log(alicia.findClothSpecified());
-  
+
+  bringClothToTheRoom() {
+    console.log("Bring the cloth to you");
+  }
+}
+
+const alicia = new Robot("demtere", "7ft", "white");
+console.log(alicia.findClothSpecified());
